4101d7f6fcade95baa4c14e3cffac80ea46f3c64
wasco <wasco.d.g@gmail.com>
2018-08-08 21:56:36 +0300

    Add some tests

--- test/crc16_test.exs
+++ test/crc16_test.exs
@@ -2,13 +2,15 @@ defmodule Crc16Test do
   use ExUnit.Case, async: true
   doctest Crc16
 
-  setup do
-    # {:ok, server_pid} = Crc16.start_link([])
-    pid = start_supervised(Crc16)
-    %{crc: pid}
+  test "Calculate crc16 code on etalon message" do
+    assert Crc16.get_crc([1,2,3,4,5,6,7,8,9,10]) == {:ok, 52555}
   end
 
-  test "Calculate crc16 code on etalon message" do
-    assert Crc16.get([1,2,3,4,5,6,7,8,9,10]) == {:ok, 52555}
+  test "Data verification with no error" do
+    assert Crc16.get_crc([1,2,3,4,5,6,7,8,9,10,205, 75]) == {:ok, 0}
+  end
+
+  test "Data verification with error" do
+    assert Crc16.get_crc([1,2,3,4,5,6,7,8,9,10,205, 76]) != {:ok, 0}
   end
 end
